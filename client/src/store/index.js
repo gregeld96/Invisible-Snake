abaa4769570c9ce4da1a38e765f5db32e8fc21c6
Handana <handanawilli0902@gmail.com>
2020-07-23 11:53:38 -0400

    Handana Williyantoro

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,11 +5,35 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    board: null,
+    row: [],
+    col: [],
+    num: 1
   },
   mutations: {
+    board (state, payload) {
+      console.log(payload)
+      state.board = payload
+    }
   },
   actions: {
+    generateBoard (context, payload) {
+      for (let i = 0; i < payload; i++) {
+        for (let j = 0; j < payload; j++) {
+          if (i % 2 === 0) {
+            this.state.col.push(this.state.num)
+            this.state.num++
+          } else {
+            this.state.col.unshift(this.state.num)
+            this.state.num++
+          }
+        }
+        this.state.row.unshift(this.state.col)
+        this.state.col = []
+      } console.log(this.state.row)
+      context.commit('board', [].concat.apply([], this.state.row))
+    }
   },
-  modules: {
+  getters: {
   }
 })
---
+++ client/src/views/home.vue
@@ -0,0 +1,79 @@
+<template>
+<div class="home" style="background-color: whitesmoke;">
+<div class="board">
+<block :data="block" class="block" v-for="(block, index) in board" :key="index"></block>
+</div>
+<button class="roll">ROLL DICE</button>
+</div>
+</template>
+
+<script>
+import block from '../components/block'
+export default {
+  name: 'home',
+  components: {
+    block
+  },
+  data () {
+    return {
+      dimension: 10,
+      board: null
+    }
+  },
+  methods: {
+    fetchBoard () {
+      this.$store.dispatch('generateBoard', this.dimension)
+    }
+  },
+  created: function () {
+    this.fetchBoard()
+    this.board = this.$store.state.board
+    this.$store.state.board = null
+  }
+}
+</script>
+
+<style>
+.home{
+    height: 600px;
+    width: 600px;
+    border: 2px solid;
+    margin-left: 310px;
+    margin-top: 30px;
+}
+
+.roll{
+    width: 400px;
+    height: 40px;
+    font-size: 18px;
+    border: 3px solid black;
+    background-color: gold;
+    color: BLACK;
+    font-weight: bold;
+}
+
+.roll:hover{
+    border: 3px solid black;
+    background-color: black;
+    color: gold;
+    font-weight: bold;
+    cursor: pointer;
+}
+
+.board{
+    height: 500px;
+    width: 500px;
+    border: 2px dashed black;
+    margin-left: 50px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    display: flex;
+    flex-wrap: wrap;
+}
+
+.block{
+    width: 10%;
+    height: 10%;
+    font-size: 14px;
+}
+</style>
